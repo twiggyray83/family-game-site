58047433f8cc946583cea7349fc8f2c69b51deca
Kevin Dornellas <dornellaskj@gmail.com>
2021-12-26 19:46:06 -0700

    created initial scripts for db and played with select

--- Sql/initDB.sql
+++ Sql/initDB.sql
@@ -1,20 +1,65 @@
+DROP TABLE IF EXISTS house_rules;
+DROP TABLE IF EXISTS bonus;
+DROP TABLE IF EXISTS playergames;
+DROP TABLE IF EXISTS tournaments;
 DROP TABLE IF EXISTS players;
 DROP TABLE IF EXISTS games;
-DROP TABLE IF EXISTS PlayerGames;
 
 create table players (
     Id serial primary Key,
     PlayerName varchar(150)
-)
+);
 
 create table games (
     Id serial primary Key,
     GameName varchar(150)
-)
+);
 
-create table PlayerGames(
-playerid int,
-CONSTRAINT fk_player
-      FOREIGN KEY(playerid) 
-	  REFERENCES players(id)
-)
+create table house_rules (
+    Id serial primary Key,
+    rule varchar,
+    gameid INT,
+    CONSTRAINT fk_game
+        FOREIGN KEY(gameid) 
+        REFERENCES games(id)
+);
+
+create table tournaments (
+    Id serial primary Key,
+    tournamentname varchar(150),
+    winner INT,
+    CONSTRAINT fk_player
+        FOREIGN KEY(winner) 
+        REFERENCES players(id)
+);
+
+create table bonus (
+    Id serial primary Key,
+    tournamentid INT,
+    playerid INT,
+    points INT,
+    CONSTRAINT fk_bonus_player
+        FOREIGN KEY(playerid) 
+        REFERENCES players(id),
+    CONSTRAINT fk_bonus_tournament
+        FOREIGN KEY(tournamentid) 
+        REFERENCES tournaments(id)
+);
+
+create table playergames(
+    Id serial primary Key,
+    playerid int,
+    gameid int,
+    score int,
+    win BOOLEAN,
+    tournamentid INT,
+    CONSTRAINT fk_pg_player
+        FOREIGN KEY(playerid) 
+        REFERENCES players(id),
+    CONSTRAINT fk_pg_game
+        FOREIGN KEY(gameid) 
+        REFERENCES games(id),
+    CONSTRAINT fk_pg_tournament
+        FOREIGN KEY(tournamentid) 
+        REFERENCES tournaments(id)
+);
--- Sql/select.sql
+++ Sql/select.sql
@@ -1,6 +1,46 @@
 
 insert into players (PlayerName)
-values ('Rachel D')
+values ('Avery D')
 RETURNING id;
 
-select * from players
+insert into games (gamename)
+values ('Yahtzee2')
+RETURNING id;
+
+insert into tournaments (tournamentname, winner)
+values ('Yahtzee1', 1)
+RETURNING id;
+
+insert into playergames (playerid, gameid, score, win, tournamentid)
+values (1, 2, 250, true, 1)
+RETURNING id;
+
+insert into bonus (tournamentid, playerid, points)
+values (1, 1, 100)
+RETURNING id;
+
+
+select 
+(sum(pg.score) + (select b.points from bonus b where b.tournamentid = 1)) / count(pg.id) as "average points"
+from playergames pg
+inner join players p on p.id = pg.playerid
+inner join tournaments t on t.id = pg.tournamentid
+inner join games g on g.id = pg.gameid 
+where p.id = 1
+and t.tournamentname = 'things' or t.tournamentname = 'Yahtzee1'
+
+select 
+distinct g.GameName
+from playergames pg
+inner join players p on p.id = pg.playerid
+inner join tournaments t on t.id = pg.tournamentid
+inner join games g on g.id = pg.gameid 
+where p.id = 1
+and t.tournamentname = 'things' or t.tournamentname = 'Yahtzee1'
+
+select * from players;
+select * from games;
+select * from tournaments;
+select * from playergames;
+select * from bonus;
+select * from house_rules;
