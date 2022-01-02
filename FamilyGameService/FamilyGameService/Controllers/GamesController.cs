5da36b1cc56bf2106004fc32a7acc4f888c24402
Kevin Dornellas <dornellaskj@gmail.com>
2022-01-02 13:13:47 -0700

    added games endpoints

---
+++ FamilyGameService/FamilyGameService/Controllers/GamesController.cs
@@ -0,0 +1,34 @@
+﻿using FamilyGameService.DTOs;
+using FamilyGameService.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FamilyGameService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GamesController : ControllerBase
+    {
+        [HttpGet(Name = "GetGames")]
+        public IEnumerable<GameResponse> Get()
+        {
+            using (var db = new postgresContext())
+            {
+                var players = new List<GameResponse>();
+                return db.Games.Select(g => new GameResponse { Id = g.Id, Gamename = g.Gamename}).ToList();
+            }
+        }
+
+        [HttpPost(Name = "CreateGame")]
+        public int POST(string gameName)
+        {
+            var game = new Models.Game { Gamename = gameName };
+            using (var db = new postgresContext())
+            {
+                db.Games.Add(game);
+                db.SaveChanges();
+                return game.Id;
+            }
+        }
+    }
+}
---
+++ FamilyGameService/FamilyGameService/DTOs/GameResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace FamilyGameService.DTOs
+{
+    public class GameResponse
+    {
+        public int Id { get; set; }
+        public string? Gamename { get; set; }
+    }
+}
